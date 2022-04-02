4edc3d3cc6661d9b1c17c09422c370588120db88
Federico Javier Loriente Pinto <federicolorientepinto@gmail.com>
2022-04-02 12:09:55 -0300

    Functional Script.

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function update(){
-   let tarifa = document.getElementById("tuTarifa").value; 
+   let tarifa = Number(document.getElementById("tuTarifa").value); 
    let porcentajePropina = document.getElementById("inputPropina").value; 
    let division = document.getElementById("divisionInput").value; 
-   console.log(tarifa, porcentajePropina, division)
+   let valorPropina = tarifa * (porcentajePropina / 100);
+   let propinaCadaUno = valorPropina / division
+   let nuevoTotalIndividual = (tarifa + propinaCadaUno) / division;
+   document.getElementById("porcentajePropina").innerHTML = porcentajePropina + "%";
+   document.getElementById("valorPropina").innerHTML = valorPropina;
+   document.getElementById("totalConPropina").innerHTML = tarifa + valorPropina;
+   document.getElementById("cantidadPersonas").innerHTML = division;
+   document.getElementById("tarifaDividida").innerHTML = nuevoTotalIndividual;
+   document.getElementById("propinaDividida").innerHTML = propinaCadaUno;
 }
 
 let app = document.getElementById("app");
